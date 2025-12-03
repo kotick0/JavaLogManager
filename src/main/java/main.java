c460033eca7b4051eba3daae8ecca72b4b54450d
kotecku <krystowskipiotr2005@gmail.com>
2025-12-03 15:13:36 +0100

    Changes to parser class

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -1,14 +1,12 @@
 import logic.FileOperations;
 import logic.NextLogResult;
-import logic.Parser;
 
 void main() {
-//    FileOperations file = new FileOperations();
-//    NextLogResult res = file.readNextLog(0);
-//    NextLogResult res2 = file.readNextLog(res.offset());
-//    NextLogResult res3 = file.readNextLog(17);
-//    System.out.println(res);
-//    System.out.println(res3); //Nie rozumiem działania
-    Parser p = new Parser();
-    p.parseLog();
+    FileOperations file = new FileOperations();
+    NextLogResult res = file.readNextLog(0);
+    NextLogResult res2 = file.readNextLog(res.offset());
+    NextLogResult res3 = file.readNextLog(res2.offset());
+    System.out.println(res);
+    System.out.println(res2); //Nie rozumiem działania
+
 }
